f6b8f0e8eb42c538a7e975eabd7adba6aae5fcd5
Sheep <ht159159@gmail.com>
2019-10-31 14:35:24 +0800

    config2

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/testMusic/'
-      : '/',
-      proxyTable: {
-        "/api":{
-          target: 'https://api.douban.com',
-          changeOrigin: true,
-          pathRewrite: {
-          '^/api': '/'
-          }
-          }
-      },
+      : '/'
   }
